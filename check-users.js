aa60ebb87457c21154fc3bc76588dc9be0df6d4c
unknown <brianzioki100@gmail.com>
2026-05-10 22:02:19 +0300

    Remove temp file

--- check-users.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({path:'.env'});
-const {PrismaClient} = require('@prisma/client');
-const p = new PrismaClient();
-p.user.findMany({select:{id:true,email:true,name:true,role:true}})
-  .then(u => { console.log(JSON.stringify(u, null, 2)); p.$disconnect(); })
-  .catch(e => { console.error(e.message); p.$disconnect(); });
